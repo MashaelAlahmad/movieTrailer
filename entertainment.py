65d73376fecf2c69be29219ca5a6163fdf7c862a
Mashael AlAhmad <42158874+MashaelAlahmad@users.noreply.github.com>
2019-01-10 04:01:04 +0300

    after modification

--- entertainment.py
+++ entertainment.py
@@ -1,16 +1,36 @@
 import media
 import fresh_tomatoes
 
-#object of movie with some info about it
+# object of movie with some info about it
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
-                        "https://imgc.allpostersimages.com/img/print/u-g-F7Z0IL0.jpg?w=550&h=550&p=0",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+                        '''https://imgc.allpostersimages.com/img/print/u-g-F7Z0IL0.
+jpg?w=550&h=550&p=0''',
+                        "https://youtu.be/azyK_k52R1w")
 
 the_hunt = media.Movie("The hunt",
-                      " A story of a man live in deimark",
-                      "https://i0.wp.com/media.fushaar.com/media/2014/07/11170774_ori.jpg?resize=329%2C495&ssl=1",
-                      "https://www.youtube.com/watch?v=rTIQmt2s_24")
+                       " A story of a man live in deimark",
+                       '''https://i0.wp.com/media.fushaar.com/media/2014/07/11170774_ori.
+jpg?resize=329%2C495&ssl=1''',
+                       "https://www.youtube.com/watch?v=rTIQmt2s_24")
+
+Holmes_Watson = media.Movie("Holmes & Watson",
+                            ''' A humorous take on Sir Arthur Conan Doyle's classic mysteries featuring
+                            Sherlock Holmes and Doctor Watson.''',
+                            '''https://m.media-amazon.com/images/M/MV5BZTNjMjAwNjQtYjE4MC00OGNlLTgzN
+DctNTE2NTBjMjRlMDgwXkEyXkFqcGdeQXVyMTYzMDM0NTU@._V1_UY268_CR4,0,1
+82,268_AL_.jpg''',
+                            "https://youtu.be/brjkpRBpFnc")
+
+A_Beautiful_Mind = media.Movie("A Beautiful Mind",
+                               '''After John Nash, a brilliant but asocial mathematician,
+                                  accepts secret work in cryptography, his life
+                                  takes a turn for the nightmarish.''',
+                               '''https://m.media-amazon.com/images/M/MV5BMzcwYWFkYzktZjAzNC00OGY1L
+WI4YTgtNzc5MzVjMDVmNjY0XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX18
+2_CR0,0,182,268_AL_.jpg''',
+                               "https://youtu.be/WFJgUm7iOKw")
+
 # this is list of movies
-movies = [toy_story, the_hunt]
+movies = [toy_story, the_hunt, Holmes_Watson, A_Beautiful_Mind]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,15 +1,16 @@
-import webbrowser
-class Movie():
-  """ this class provide a way to store a movie related infrmation"""
-  VALID_RATING = ["G", "PG", "PG-13", "R"]
-  #constructor
-  def __init__(self, movie_title, movie_storyline, poster_image,
-               trailer_youtube):
-    self.title = movie_title
-    self.storyline = movie_storyline
-    self.poster_image_url = poster_image
-    self.trailer_youtube_url = trailer_youtube
 
-#fct to play movie trailer
-  def show_trailer(self):
-    webbrowser.open(self.trailer_youtube_url)
+class Movie():
+    """ this class provide a way to store a movie related infrmation
+
+    Attributes
+    title (str):  represent the title of movie
+    storyline (str): represent the story of movie
+    poster_image_url (str): it's a link for poster poster_image
+    trailer_youtube_url (str): it's a link for movie """
+# constructor
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
